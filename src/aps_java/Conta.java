8cd38e6f4da705720e8a716c8150475a4e7bd889
Gil <lancemaker@gmail.com>
2018-05-17 18:11:30 -0300

    so um comentario.

--- src/aps_java/Conta.java
+++ src/aps_java/Conta.java
@@ -16,6 +16,8 @@ Construtor: que receba o número da conta, código da agência,
 nome do cliente e CPF; Todos os atributos deverão ter get, porém o saldo,
  o número da conta e CPF não poderão ter sets públicos. 
  */
+
+//checar com o professor se ela precisa ser abstract, eu acho que sim.
 public class Conta {
     private String nomeCliente, CPF, numConta, agencia;
     private double saldo;
