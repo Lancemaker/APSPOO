3096f31ec6d9edf4cbe93cf7f4a80311f75a90c1
Adriano <adriano_paiv@hotmail.com>
2018-05-27 00:22:45 -0300

    Não consegui testar os métodos, pois não consegui chamar. Tb to meio perdido no menu, não sei se fiz certo, se a ordem ta certa. NEED HELP GUYS

--- src/aps_java/APS_Java.java
+++ src/aps_java/APS_Java.java
@@ -43,43 +43,76 @@ public class APS_Java {
         int numConta, agencia, opção;
         double saldo, limite, porcentagemRendimento;     
         
-        System.out.println("Digite o nome do cliente: ");
-        nomeCliente = entrada.nextLine();
-        System.out.println("Digite o CPF do cliente: ");
-        CPF = entrada.nextLine();
-        System.out.println("Digite o nome do gerente: ");
-        gerente = entrada.nextLine();
-        System.out.println("Digite o numero da conta: ");
-        numConta = entrada.nextInt();
-        System.out.println("Digite o codigo da agencia: ");
-        agencia = entrada.nextInt();
-        System.out.println("Digite o saldo: ");
-        saldo = entrada.nextDouble();
-        System.out.println("Digite o limite do cartao: ");
-        limite = entrada.nextDouble();
-        System.out.println("Digite a porcentagem de Rendimento: ");
-        porcentagemRendimento = entrada.nextDouble();
+//        System.out.println("Digite o nome do cliente: ");
+//        nomeCliente = entrada.nextLine();
+//        System.out.println("Digite o CPF do cliente: ");
+//        CPF = entrada.nextLine();
+//        System.out.println("Digite o nome do gerente: ");
+//        gerente = entrada.nextLine();
+//        System.out.println("Digite o numero da conta: ");
+//        numConta = entrada.nextInt();
+//        System.out.println("Digite o codigo da agencia: ");
+//        agencia = entrada.nextInt();
+//        System.out.println("Digite o saldo: ");
+//        saldo = entrada.nextDouble();
+//        System.out.println("Digite o limite do cartao: ");
+//        limite = entrada.nextDouble();
+//        System.out.println("Digite a porcentagem de Rendimento: ");
+//        porcentagemRendimento = entrada.nextDouble();
+        
+        do {
+        
+        System.out.println("Informe a opção desejada: ");
+        System.out.println("--------------------- Opções ---------------------");
+        System.out.println("1 - Adicionar Cliente");
+        System.out.println("2 - Remover Cliente");
+        System.out.println("3 - Editar Cliente");
+        System.out.println("4 - Buscar por Nome");
+        System.out.println("5 - Buscar por CPF");
+        System.out.println("6 - Sair");
         
         opção = entrada.nextInt();
+        entrada.nextLine();
         
         switch(opção){
                 case 1:
-                    System.out.println("Adicionar");                    
+                    adicionarConta();
+                    System.out.println("Informe a opção desejada: ");
+                    System.out.println("--------------------- Opções ---------------------");
+                    System.out.println("1 - Conta Corrente");
+                    System.out.println("2 - Conta Especial");
+                    System.out.println("3 - Conta Poupança");
+                    
+                    opção = entrada.nextInt();
+                    entrada.nextLine();
+                    
+                    switch(opção){
+                        case 1:
+                            System.out.println(ContaCorrente.class);
+                            break;
+                        case 2:
+                            System.out.println(ContaEspecial.class);
+                            break;
+                        case 3: 
+                            System.out.println(ContaPoupanca.class);
+                            break;
+                    }
+                    
                     break;
                 case 2:
-                    System.out.println("Remover");
+                    remover();
                     break;
                 case 3:
-                    System.out.println("Editar");
+                    editar();
                     break;
                 case 4:
-                    System.out.println("Buscar por Nome");
+                    buscarPorNome();
                     break;
                 case 5:
-                     System.out.println("Buscar por CPF");
+                     buscarPorCPF();
                      break;
-        
+            }
         }
-        
+        while (opção != 5);
     }
 }
