430f3d17b3d6b37c9115d7059c5a06de9c27885d
xmightyboyx <39386694+xmightyboyx@users.noreply.github.com>
2018-05-26 22:10:34 -0300

    Update APS_Java.java

--- src/aps_java/APS_Java.java
+++ src/aps_java/APS_Java.java
@@ -1,13 +1,47 @@
 package aps_java;
 import java.util.Scanner;
 public class APS_Java {
+    
+    
+   public static void menu(){
+        System.out.println("Informe a opção desejada: ");
+        System.out.println("--------------------- Opções ---------------------");
+        System.out.println("1 - Adicionar Cliente");
+        System.out.println("2 - Remover Cliente");
+        System.out.println("3 - Editar Cliente");
+        System.out.println("4 - Buscar por Nome");
+        System.out.println("5 - Buscar por CPF");
+        System.out.println("6 - Sair");
+   }
+   
+   public static void adicionarConta(){
+       
+   }
+   
+   public static void remover(){
+       
+   }
+   
+   public static void editar(){
+       
+   }
+   
+   public static void buscarPorNome(){
+       
+   }
+   
+   public static void buscarPorCPF(){
+       
+   }
+   
+   
     public static void main(String[] args) {
         
         Scanner entrada = new Scanner(System.in);
         
         String nomeCliente, CPF, gerente;
-        int numConta, agencia;
-        double saldo, limite, porcentagemRendimento;       
+        int numConta, agencia, opção;
+        double saldo, limite, porcentagemRendimento;     
         
         System.out.println("Digite o nome do cliente: ");
         nomeCliente = entrada.nextLine();
@@ -26,7 +60,26 @@ public class APS_Java {
         System.out.println("Digite a porcentagem de Rendimento: ");
         porcentagemRendimento = entrada.nextDouble();
         
+        opção = entrada.nextInt();
+        
+        switch(opção){
+                case 1:
+                    System.out.println("Adicionar");                    
+                    break;
+                case 2:
+                    System.out.println("Remover");
+                    break;
+                case 3:
+                    System.out.println("Editar");
+                    break;
+                case 4:
+                    System.out.println("Buscar por Nome");
+                    break;
+                case 5:
+                     System.out.println("Buscar por CPF");
+                     break;
+        
+        }
         
     }
-    
 }
